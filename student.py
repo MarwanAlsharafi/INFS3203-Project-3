2784cc134d52a7139e5c35f266f5d0f94097b131
huzaifa-60095630 <60084228@udst.edu.qa>
2022-10-17 17:44:18 +0300

    Added Password Tests

--- student.py
+++ student.py
@@ -2,8 +2,7 @@ import base64
 import string
 import random
 
-class Student:
-    def pass_random():
+def pass_random():
     
         password = string.ascii_letters + string.digits
         result_str = ''.join((random.choice(password) for i in range(8)))
@@ -11,9 +10,11 @@ class Student:
         passEn = result_str.encode("utf-8")
         encoded = base64.b64encode(passEn)
         return encoded 
+        
+class Student:
     
     def __str__(self):
-        return f"{self.id}, {self.name}, {self.age}, {self.pass}"
+        return f"{self.id}, {self.name}, {self.age}, {self.password}"
 
     def __init__(self, name, age, id):
         if type(name) != str:
@@ -29,4 +30,4 @@ class Student:
         self.name = name
         self.age = age
         self.id = id
-        self.pass = pass_random()
+        self.password = pass_random()
